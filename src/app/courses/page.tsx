dd798467d216ead86ad5bdf02063660a316e142e
narit-sara <naritnaphrae@gmail.com>
2026-08-17 16:11:58 +0700

    Complete lab assignment

---
+++ src/app/courses/page.tsx
@@ -0,0 +1,47 @@
+
+
+export default function CoursePage(){   
+    type Course = {
+  id: number;
+  code: string;
+  title: string;
+  credits: number;
+  isOpen: boolean;
+};
+
+const courses: Course[] = [
+  {
+    id: 1,
+    code: "10301231",
+    title: "Web Technology",
+    credits: 3,
+    isOpen: true,
+  },
+  {
+    id: 2,
+    code: "10301232",
+    title: "Database Systems",
+    credits: 3,
+    isOpen: false,
+  },
+];
+
+  return (
+<div>
+    <main>
+    {courses.map((course, index) => (
+      <article key={course.id}>
+        <h2>{index+1}.{course.title}</h2>
+        <p>รหัสวิชา: {course.code}</p>
+        <p>{course.credits} หน่วยกิต</p>
+        <p>{course.isOpen ? "เปิดลงทะเบียน" : "ปิดลงทะเบียน"}</p>
+      </article>
+      ))}
+    </main>
+
+
+</div>
+);
+
+
+}
